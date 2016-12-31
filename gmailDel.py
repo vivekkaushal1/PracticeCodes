a3a431b92532f1eb496073aac4237c035af26a44
Vivek Kaushal <vivekkaushal27@yahoo.co.in>
2016-12-31 14:21:49 -0800

    Delete Email

---
+++ gmailDel.py
@@ -0,0 +1,21 @@
+import imaplib
+import sys, getpass
+
+pw = getpass.getpass('Password:')
+server = imaplib.IMAP4_SSL('imap.gmail.com')
+connection_message = server.login(sys.argv[1], pw)
+print(connection_message)
+
+print (server.select("inbox"))
+
+#server.select("ALL MAIL")
+sender ='kundangarg551@gmail.com'
+result_status, email_ids = server.search(None, '(FROM "%s")' % sender)
+emails = email_ids[0].split()
+
+print ('%d emails found'%len(emails))
+for x in emails:
+    server.store(x,'+X-GM-LABELS', '\\Trash')
+    pass
+print ("Deleted %d messages. Closing connection & logging out."%len(emails))
+server.logout()
